0386a02a454ab10e2c9c43ec21d5dceee2f8e5e0
KunChi <kuo23201199@gmail.com>
2020-02-24 09:00:17 +0800

    init questionnaire app

---
+++ medical_questionnaire/questionnaire/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def startQuestion(request):
+    pass
+
+def getQuestion(request):
+    pass
+
+def selectAnswer(request):
+    pass
+
+def getSummary(request):
+    pass
+
+def exit(request):
+    pass
